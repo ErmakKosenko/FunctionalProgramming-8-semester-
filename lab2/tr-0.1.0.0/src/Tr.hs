426785f82af5e30e3ae11ec88572e36f99324801
ErmakKosenko <ermak_kosenko@mail.ru>
2021-03-20 18:35:00 +0300

    Add Data.Map and Tail Call to lab2

--- lab2/tr-0.1.0.0/src/Tr.hs
+++ lab2/tr-0.1.0.0/src/Tr.hs
@@ -3,6 +3,8 @@ module Tr
     , tr
     ) where
 
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 import Data.Maybe
 
 type CharSet = String
@@ -26,11 +28,24 @@ type CharSet = String
 -- It's up to you how to handle the first argument being the empty string, or
 -- the second argument being `Just ""`, we will not be testing this edge case.
 
-replacementMap :: CharSet -> CharSet -> [(Char, Char)]
-replacementMap [] _ = []
-replacementMap (x : firstOthers) [y] = (x, y) : replacementMap firstOthers [y]
-replacementMap (x : firstOthers) (y : secondOthers) = (x, y) : replacementMap firstOthers secondOthers
+
+-- IMPLEMENTATION:
+
+-- old function:
+-- replacementMap :: CharSet -> CharSet -> Map Char Char
+-- replacementMap [] _ = Map.empty
+-- replacementMap (x : firstOthers) [y] = Map.insert x y (replacementMap firstOthers [y])
+-- replacementMap (x : firstOthers) (y : secondOthers) = Map.insert x y (replacementMap firstOthers secondOthers)
+
+-- new function with tail call
+replacementMap :: CharSet -> CharSet -> Map Char Char
+replacementMap first second = helper first second Map.empty
+    where
+        insertIfNotExist x y output = if Map.member x output then output else Map.insert x y output
+        helper [] _ output = output
+        helper (x : firstOthers) [y] output = helper firstOthers [y] (insertIfNotExist x y output)
+        helper (x : firstOthers) (y : secondOthers) output = helper firstOthers secondOthers (insertIfNotExist x y output)
 
 tr :: CharSet -> Maybe CharSet -> String -> String
-tr first (Just second) = map (\x -> Data.Maybe.fromMaybe x (lookup x (replacementMap first second)))
+tr first (Just second) = map (\x -> Data.Maybe.fromMaybe x (Map.lookup x (replacementMap first second)))
 tr first Nothing = filter (`notElem` first)
